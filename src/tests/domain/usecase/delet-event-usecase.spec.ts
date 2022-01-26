e9c68f5f390462ca1e7d0d53d95e996e4c84f79d
Racchel <racchelvelasco@gmail.com>
2022-01-26 11:51:26 -0300

    ensure DeleteEvent throws if userId is invalid

--- src/tests/domain/usecase/delet-event-usecase.spec.ts
+++ src/tests/domain/usecase/delet-event-usecase.spec.ts
@@ -1,12 +1,21 @@
 /* eslint-disable no-plusplus */
 // eslint-disable-next-line max-classes-per-file
+type GroupUser = {
+   id: string;
+   permission: string;
+};
+
+type Group = {
+   users: [GroupUser];
+};
+
 interface ILoadGroupRepository {
-   load: (input: { eventId: string }) => Promise<any>;
+   load: (input: { eventId: string }) => Promise<Group | undefined>;
 }
 
 class LoadGroupRepository implements ILoadGroupRepository {
    eventId?: string;
-   async load(input: { eventId: string }): Promise<any> {
+   async load(input: { eventId: string }): Promise<Group | undefined> {
       return undefined;
    }
 }
@@ -14,9 +23,11 @@ class LoadGroupRepository implements ILoadGroupRepository {
 class LoadGroupRepositoryMock implements ILoadGroupRepository {
    eventId?: string;
    callsCount = 0;
-   output: any = 'any_value';
+   output?: Group = {
+      users: [{ id: 'any_user_id', permission: 'any' }],
+   };
 
-   async load({ eventId }: { eventId: string }): Promise<any> {
+   async load({ eventId }: { eventId: string }): Promise<Group | undefined> {
       this.eventId = eventId;
       this.callsCount++;
       return this.output;
@@ -26,9 +37,17 @@ class LoadGroupRepositoryMock implements ILoadGroupRepository {
 class DeleteEvent {
    constructor(private readonly loadGroupRepository: LoadGroupRepository) {}
 
-   async perform({ id }: { id: string; userId: string }): Promise<void> {
+   async perform({
+      id,
+      userId,
+   }: {
+      id: string;
+      userId: string;
+   }): Promise<void> {
       const group = await this.loadGroupRepository.load({ eventId: id });
       if (group === undefined) throw new Error();
+      if (group.users.find((user) => user.id === userId) === undefined)
+         throw new Error();
    }
 }
 type SutTypes = {
@@ -60,6 +79,17 @@ describe('DeleteEvent', () => {
       loadGroupRepository.output = undefined;
 
       const promise = sut.perform({ id, userId });
+
+      await expect(promise).rejects.toThrowError();
+   });
+
+   it('should throw if userId is invalid', async () => {
+      const { sut, loadGroupRepository } = makeSut();
+      loadGroupRepository.output = {
+         users: [{ id: 'any_user_id', permission: 'any' }],
+      };
+
+      const promise = sut.perform({ id, userId: 'invalid_id' });
 
       await expect(promise).rejects.toThrowError();
    });
