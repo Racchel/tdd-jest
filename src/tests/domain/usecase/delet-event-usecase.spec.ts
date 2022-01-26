215b5831c3b392e66238a5f6e626e9e8f83ac910
Racchel <racchelvelasco@gmail.com>
2022-01-26 11:17:21 -0300

    move SUT creation to a factory method

--- src/tests/domain/usecase/delet-event-usecase.spec.ts
+++ src/tests/domain/usecase/delet-event-usecase.spec.ts
@@ -25,13 +25,23 @@ class DeleteEvent {
       await this.loadGroupRepository.load({ eventId: id });
    }
 }
+type SutTypes = {
+   sut: DeleteEvent;
+   loadGroupRepository: LoadGroupRepositoryMock;
+};
+
+const makeSut = (): SutTypes => {
+   const loadGroupRepository = new LoadGroupRepositoryMock();
+   const sut = new DeleteEvent(loadGroupRepository);
+   return { sut, loadGroupRepository };
+};
 
 describe('DeleteEvent', () => {
+   const id = 'any_event_id';
+   const userId = 'any_user_id';
+
    it('should get group data', async () => {
-      const id = 'any_event_id';
-      const userId = 'any_user_id';
-      const loadGroupRepository = new LoadGroupRepositoryMock();
-      const sut = new DeleteEvent(loadGroupRepository);
+      const { sut, loadGroupRepository } = makeSut();
 
       await sut.perform({ id, userId });
 
